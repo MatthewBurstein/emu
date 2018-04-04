ab092f586da3319fb462b5c1cf2a2b34bcbd797d
kotpsot <zygmunt.justyna@gmail.com>
2018-04-04 12:40:01 +0100

    Fixed missing }.|

--- src/parser.js
+++ src/parser.js
@@ -6,13 +6,14 @@ const parser = function(tokens) {
       output.push(functionNode)
       functionNode.type = token.type
       functionNode.name = token.value}
-    if (token.type === "open paren")
+    if (token.type === "open paren"){
       functionNode.arguments = []}
-    if(token.type === 'string')
+    if(token.type === 'string'){
       functionNode.arguments.push({
         type: token.type,
         value: token.value
       })
+    }
   })
   return output
 };
