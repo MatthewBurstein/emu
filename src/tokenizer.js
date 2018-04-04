686ecc1b5cf73f4b9447edca99a0b9652d636485
Noel Vock <noel1uk@gmail.com>
2018-04-04 15:52:55 +0100

    in progress

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,19 +1,26 @@
 ((exports) => {
   const tokens = [
     { match: /\bsay\b/, type: 'function', value: 'console.log' },
-    { match: /\b[a-z0-9]+\b/i, type: 'string', value: 'some value' }
+    { match: /([\w])/, type: 'string', value: 'some value' },
+    { match: /\(/, type: 'open paren', value: '(' }
   ]
 
   function tokenize(input) {
     let output = []
-
     tokens.forEach(token => {
       if (input.match(token.match)) {
-        output.push({
-          type: token.type,
-          value: token.match
-        })
+        console.log(input.match(token.match)[0]);
+          output.push({
+            type: token.type,
+            value: input.match(token.match)[0]
+          })
+          var match = input.match(token.match)[0]
+          input = input.slice(match.length).trim()
+          console.log(input.match(token.match)[0]);
+        // }
+        console.log(output);
       }
     })
   }
+  exports.tokenize = tokenize
 })(this)
