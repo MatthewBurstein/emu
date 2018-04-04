d4bc5daec484788450c8f8181b9c2088200864f4
kotpsot <zygmunt.justyna@gmail.com>
2018-04-04 12:39:01 +0100

    Delete console.logs.

--- src/parser.js
+++ src/parser.js
@@ -3,20 +3,16 @@ const parser = function(tokens) {
   let functionNode = {};
   tokens.forEach( token => {
     if (token.type === "function"){
-      console.log(token)
       output.push(functionNode)
       functionNode.type = token.type
       functionNode.name = token.value}
     if (token.type === "open paren")
-      {console.log(token)
       functionNode.arguments = []}
     if(token.type === 'string')
-      {console.log(token)
       functionNode.arguments.push({
         type: token.type,
         value: token.value
       })
-      console.log(output)}
   })
   return output
 };
