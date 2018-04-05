4bdca61d93b923376b0ce679aa5b32d6991667b2
kotpsot <zygmunt.justyna@gmail.com>
2018-04-05 15:57:03 +0100

    create a Dictinary constructor.

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,3 +1,21 @@
-const say = function(string) {
-  return string
+const Dictionary = function () {
+}
+
+Dictionary.prototype.say = function(node) {
+  if (node.args.length === 0) {
+    return ''
+  }
+  if (node.args.length === 1) {
+    return node.args[0].value
+  }
+  else {
+    var joinedString = ''
+    for(i=0; i < args.length; i++){
+      joinedString += node.args[i].value
+      if (i !== args.length -1 ) {
+        joinedString += ' '
+      }
+    }
+    return joinedString
+  }
 }
--- src/interpret.js
+++ src/interpret.js
@@ -1,23 +1,9 @@
 const interpret = function(tree) {
+  dictionary = new Dictionary()
   while (tree.length > 0) {
     node = tree.shift();
     if (node.name === 'say') {
-      if (node.args.length === 0) {
-        return ''
-      }
-      if (node.args.length === 1) {
-        return node.args[0].value
-      }
-      else {
-        var joinedString = ''
-        for(i=0; i < args.length; i++){
-          joinedString += node.args[i].value
-          if (i !== args.length -1 ) {
-            joinedString += ' '
-          }
-        }
-        return joinedString
-      }
+      return dictionary.say(node)
     }
   }
 };
