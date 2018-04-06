d9eed302a54c788a45724dcd5415992f333f8a3e
kotpsot <zygmunt.justyna@gmail.com>
2018-04-06 14:36:02 +0100

    Add comment in interpreter for clarification.

--- src/interpret.js
+++ src/interpret.js
@@ -2,6 +2,6 @@ const interpret = function(tree) {
   dictionary = new Dictionary()
   while (tree.length > 0) {
     node = tree.shift();
-    return dictionary[node.name](node)
+    return dictionary[node.name](node) // === eg. dictionary.say(node) is the same as dictionary[say](node)
   }
 };
