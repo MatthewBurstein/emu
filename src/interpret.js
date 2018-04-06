392769222f19bcb9450453c7e2b91c7aaea975e6
Laura Chan <laurachan205@gmail.com>
2018-04-06 17:23:29 +0100

    Add factory method in dictionary.js and apply in interpret.js

--- src/interpret.js
+++ src/interpret.js
@@ -1,6 +1,6 @@
 ;((exports) => {
   const interpret = function(tree) {
-    dictionary = new Dictionary()
+    dictionary = Dictionary.new()
     while (tree.length > 0) {
       node = tree.shift();
       return dictionary[node.name](node) // === eg. dictionary.say(node) is the same as dictionary[say](node)
