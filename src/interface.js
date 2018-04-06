e55edb602fc3dc31c7d69bf1fffad3cb7997b830
kotpsot <zygmunt.justyna@gmail.com>
2018-04-06 16:28:38 +0100

    Website displays tokens.

--- src/interface.js
+++ src/interface.js
@@ -2,13 +2,21 @@ $( document ).ready(function() {
 
   $('.write_code').submit(function(e) {
     e.preventDefault();
-    var value = $( ".write_code_form" ).val();
-    var tokens = tokenize(value)
-    var parsed = parse(tokens)
-    var interpreted = interpret(parsed)
-    console.log(interpreted)
-
+    let value = $( ".write_code_form" ).val();
+    let tokens = tokenize(value)
+    showTokens(tokens)
+    let parsed = parse(tokens)
+    let interpreted = interpret(parsed)
     $('.output').text(interpreted);
   })
+
+  showTokens = function(tokens) {
+    $('.tokens').append('<div class="headline">Tokens: </div>')
+    tokens.forEach( token => {
+      tokenString = "type: " + token.type + ", value: " + token.value + " ;"
+      $( "<br>" ).appendTo( ".tokens" )
+      $('.tokens').append(`${tokenString}`)
+    })
+  }
 
 })
