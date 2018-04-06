97e6be84861c5f728ab2d5576600d0dd096a9bcb
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-06 21:08:02 +0100

    refactor: add mocks to interpretSpec()

--- spec/interpretSpec.js
+++ spec/interpretSpec.js
@@ -1,152 +1,21 @@
 describe('interpret()', function() {
-  let tree, functionNode;
-
+  let testFunctionSpy, functionNode, tree;
   beforeEach(() => {
+    testFunctionSpy = jasmine.createSpy('testFunction').and.returnValue('success')
     spyOn(Dictionary, 'new').and.returnValue({
-      testFunction: () => { return 'returnValue' }
+      testFunction: testFunctionSpy
     })
+    functionNode = { name: 'testFunction' }
+    tree = [functionNode]
+  })
+
+  it('instantiates a dictionary object', () => {
+    interpret(tree)
+    expect(Dictionary.new).toHaveBeenCalledWith()
   })
 
   it('understands a tree with one expression', () => {
-    functionNode = { name: 'testFunction' }
-    tree = [functionNode]
-
-    expect(interpret(tree)).toEqual('returnValue')
+    interpret(tree)
+    expect(testFunctionSpy).toHaveBeenCalledWith(functionNode)
   })
-
-
-  //
-  // it('understands say with zero arguments', function() {
-  //   let functionNode = new FunctionNode('say', [])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual('')
-  // })
-  //
-  // it('understands say with one argument', function() {
-  //   let stringNode = new StringNode('hello world')
-  //   let functionNode = new FunctionNode('say', [stringNode])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual('hello world')
-  // })
-  //
-  // it('understands say with two arguments', function() {
-  //   let stringNode1 = new StringNode('hello world')
-  //   let stringNode2 = new StringNode('goodbye cruel world')
-  //   let functionNode = new FunctionNode('say', [stringNode1, stringNode2])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual('hello world goodbye cruel world')
-  // })
-  //
-  // it('understands add with two arguments', function() {
-  //   let integerNode1 = new IntegerNode(1)
-  //   let integerNode2 = new IntegerNode(2)
-  //   let functionNode = new FunctionNode('add', [integerNode1, integerNode2])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(3)
-  // })
-  //
-  // it('understands add with zero arguments', function() {
-  //   let functionNode = new FunctionNode('add', [])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(0)
-  // })
-  //
-  // it('understands add with one argument', function() {
-  //   let integerNode1 = new IntegerNode(1)
-  //   let functionNode = new FunctionNode('add', [integerNode1])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(1)
-  // })
-  //
-  // it('understand assignVariable with two arguments', function() {
-  //   let stringNode = new StringNode('test')
-  //   let integerNode = new IntegerNode(1)
-  //   let functionNode = new FunctionNode('assignVariable', [stringNode, integerNode])
-  //   let newTokenLex = {
-  //     regEx: /^test/, type: 'variable', value: 1, variableType: 'integer'
-  //   }
-  //
-  //   let tree = [functionNode]
-  //
-  //   interpret(tree)
-  //   expect(tokenDictionary).toContain(newTokenLex)
-  // })
-  //
-  // it('understands subtract with one arguments', function() {
-  //   let integerNode1 = new IntegerNode(1)
-  //   let functionNode = new FunctionNode('subtract', [integerNode1])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(1)
-  // })
-  //
-  // it('understands subtraction with two arguments', function() {
-  //   let integerNode1 = new IntegerNode(2)
-  //   let integerNode2 = new IntegerNode(1)
-  //   let functionNode = new FunctionNode('subtract', [integerNode1, integerNode2])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(1)
-  // })
-  //
-  // it('understands subtraction with 0 arguments', function() {
-  //   let functionNode = new FunctionNode('subtract', [])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(0)
-  // })
-  //
-  // it('understands multiply with two arguments', function() {
-  //   let integerNode1 = new IntegerNode(5)
-  //   let integerNode2 = new IntegerNode(4)
-  //   let functionNode = new FunctionNode('multiply', [integerNode1, integerNode2])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(20)
-  // })
-  //
-  // it('understands multiply with one argument', function() {
-  //   let integerNode1 = new IntegerNode(5)
-  //   let functionNode = new FunctionNode('multiply', [integerNode1])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(5)
-  // })
-  //
-  // it('understands multiply with zero arguments', function() {
-  //   let functionNode = new FunctionNode('multiply', [])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(0)
-  // })
-  //
-  // it('understands modulo with 2 arguments', function() {
-  //   let integerNode1 = new IntegerNode(5)
-  //   let integerNode2 = new IntegerNode(2)
-  //   let functionNode = new FunctionNode('modulo', [integerNode1, integerNode2])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(1)
-  // })
-  //
-  // it('understands modulo with 1 argument', function() {
-  //   let integerNode = new IntegerNode(4)
-  //   let functionNode = new FunctionNode('modulo', [integerNode])
-  //   let tree = [functionNode]
-  //
-  //   expect(interpret(tree)).toEqual(4)
-  // })
-  //
-  //   it('understand modulo with 0 arguments', function() {
-  //     let functionNode = new FunctionNode('modulo', [])
-  //     let tree = [functionNode]
-  //
-  //     expect(interpret(tree)).toEqual(0)
-  //   })
 })
