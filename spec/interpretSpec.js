c289e07107e4be52298460427ef82c9a3fb9f8bb
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-07 22:00:31 +0100

    Initial recursion solution

--- spec/interpretSpec.js
+++ spec/interpretSpec.js
@@ -5,18 +5,22 @@ describe('interpret()', function() {
     // spyOn(Dictionary, 'new').and.returnValue({
     //   testFunction: testFunctionSpy
     // })
-    functionNode = { name: 'testFunction' }
-    tree = [functionNode]
+    // functionNode = { name: 'testFunction' }
+    // tree = [functionNode]
   })
 
   it('instantiates a dictionary object', () => {
-    interpret(tree)
-    expect(Dictionary.new).toHaveBeenCalledWith()
+    // interpret(tree)
+    // expect(Dictionary.new).toHaveBeenCalledWith()
   })
 
   it('understands a tree with one expression', () => {
-    interpret(tree)
-    expect(testFunctionSpy).toHaveBeenCalledWith(functionNode)
+    intNode1 = IntegerNode.new(1)
+    intNode2 = IntegerNode.new(2)
+    funcNode = FunctionNode.new('add', [intNode1, intNode2])
+    tree = [funcNode]
+
+    expect(interpret(tree)).toEqual([3])
   })
 
   describe('when passed nested AST', () => {
@@ -24,13 +28,13 @@ describe('interpret()', function() {
       // 'add<1 add<2 3>>'
       intNode1 = IntegerNode.new(1)
       intNode2 = IntegerNode.new(2)
-      intNode3 = IntegerNode.new(3)      
+      intNode3 = IntegerNode.new(3)
       innerFuncNode = FunctionNode.new('add', [intNode2, intNode3])
       outerFuncNode = FunctionNode.new('add', [intNode1, innerFuncNode])
 
       tree = [outerFuncNode]
 
-      expect(interpret(tree)).toEqual(6)
+      expect(interpret(tree)).toEqual([6])
     })
   })
 })
--- src/interpret.js
+++ src/interpret.js
@@ -1,19 +1,16 @@
 ;((exports) => {
   const interpret = function(tree) {
     dictionary = Dictionary.new()
-    while (tree.length > 0) {
-      node = tree.shift();
+    tree = tree.map((node, idx) => {
       if (node.type === 'function') {
-        interpret(node.args)
-        node = dictionary[node.name](node) // === eg. dictionary.say(node) is the same as dictionary[say](node)
+        node.args = interpret(node.args)
+        return dictionary[node.name](node)
+      } else {
+        return node.value
       }
-      else {
-        break;
-        // for literal nodes, replace node with value
-        // REVIEW! -> Done in Dictionary, need to be in Interpreter
-      }
-    }
-  };
+    })
+    return tree
+  }
 
   exports.interpret = interpret
 })(this)
