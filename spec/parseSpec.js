94da06a04f59cb4ed9e7f86923fbcb7f8bd482c7
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-07 15:17:31 +0100

    recurssion for nested functions done in parser, next: interpreter

--- spec/parseSpec.js
+++ spec/parseSpec.js
@@ -48,28 +48,40 @@ describe("Parse", function(){
     expect(StringNode.new).toHaveBeenCalledWith('bye world')
   })
 
-  // it('understands a nested function with one argument', () => {
-  //   const tokens = [
-  //     {type: 'function', value: 'say'},
-  //     {type: 'open paren', value: '<'},
-  //     {type: 'function', value: 'say'},
-  //     {type: 'open paren', value: '<'},
-  //     {type: 'string', value: 'hello world'},
-  //     {type: 'close paren', value: '>'},
-  //     {type: 'string', value: 'bye world'},
-  //     {type: 'close paren', value: '>'}
-  //   ]
-  //
-  //   spyOn(StringNode, "new").and.returnValues('hello world', 'bye world');
-  //   spyOn(FunctionNode, 'new').and.returnValue({args: []})
-  //   let tree = [{args:
-  //     [{args: ['hello world']},
-  //     'bye world']
-  //   }]
-  //
-  //   expect(parse(tokens)).toEqual(tree)
-  //   expect(FunctionNode.new).toHaveBeenCalledWith("say", [])
-  //   expect(StringNode.new).toHaveBeenCalledWith('hello world')
-  //   expect(StringNode.new).toHaveBeenCalledWith('bye world')
-  // })
+  it('understands a nested function with one argument', () => {
+    const tokens = [
+      { type: 'function', value: 'say' },
+      { type: 'open paren', value: '<' },
+      { type: 'string', value: 'hello world' },
+      { type: 'function', value: 'sayAgain' },
+      { type: 'open paren', value: '<' },
+      { type: 'string', value: 'bye world' },
+      { type: 'close paren', value: '>' },
+      { type: 'close paren', value: '>' }
+    ]
+
+    // Mocked stuff, later
+    // spyOn(StringNode, "new").and.returnValues('hello world', 'bye world');
+    // spyOn(FunctionNode, 'new').and.returnValue({args: []})
+    // let tree = [{args:
+    //   [{args: ['hello world']},
+    //   'bye world']
+    // }]
+    let stringNode1 = StringNode.new('hello world')
+    let stringNode2 = StringNode.new('bye world')
+    let innerFunction = FunctionNode.new('sayAgain', [stringNode2])
+    let outerFunction = FunctionNode.new('say', [stringNode1, innerFunction])
+    let tree = [outerFunction]
+
+    expect(parse(tokens)).toEqual(tree)
+  })
 })
+
+Expected undefined to equal[FunctionNode(
+  { name: 'say',
+   args: 
+  [StringNode({ type: 'string', value: 'hello world' }),
+   FunctionNode(
+     { name: 'sayAgain', 
+       args: [StringNode({ type: 'string', value: 'bye world' })],
+       type: 'function' })], type: 'function' })].
