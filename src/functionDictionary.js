0658c8e1d98f644e2d671456814ddd97869cf9f8
kotpsot <zygmunt.justyna@gmail.com>
2018-04-10 11:25:19 +0100

    Call _convertVariablesToValues in functionDictionary methods.

--- src/functionDictionary.js
+++ src/functionDictionary.js
@@ -7,28 +7,33 @@
   }
 
   FunctionDictionary.prototype.say = function(node) {
-    return node.args.join(' ')
+    const arguments = this._convertVariablesToValues(node.args)
+    return arguments.join(' ')
   }
 
   FunctionDictionary.prototype.add = function(node) {
-    if (node.args.length === 0 ) { return 0 }
-    return node.args.reduce((sum, value) => sum + value)
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0 ) { return 0 }
+    return arguments.reduce((sum, value) => sum + value)
   }
 
   FunctionDictionary.prototype.subtract = function(node) {
-    if (node.args.length === 0 ) { return 0 }
-    return node.args.reduce(function(accumulator, elem) { return accumulator - elem })
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0 ) { return 0 }
+    return arguments.reduce(function(accumulator, elem) { return accumulator - elem })
   }
 
   FunctionDictionary.prototype.multiply = function(node) {
-    if (node.args.length === 0 ) { return 0 }
-    return node.args.reduce(function(accumulator, elem) { return accumulator * elem })
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0 ) { return 0 }
+    return arguments.reduce(function(accumulator, elem) { return accumulator * elem })
   }
 
   FunctionDictionary.prototype.modulo = function(node) {
-    if (node.args.length === 0 ) { return 0 }
-    if (node.args.length === 1 ) { return node.args[0] }
-    return node.args[0] % node.args[1]
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0 ) { return 0 }
+    if (arguments.length === 1 ) { return arguments[0] }
+    return arguments[0] % arguments[1]
   }
 
   FunctionDictionary.prototype.assignVariable = function(node) {
@@ -59,40 +64,44 @@
   }
 
   FunctionDictionary.prototype.isGreaterThan = function(node) {
-    if (node.args.length === 0) {
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0) {
       return 'there is nothing to compare'
     }
-    if (node.args.length === 1) {
+    if (arguments.length === 1) {
       return 'pass two arguments'
     }
-    return node.args[0] > node.args[1] ? 'yes' : 'no'
+    return arguments[0] > arguments[1] ? 'yes' : 'no'
   }
 
   FunctionDictionary.prototype.isEqual = function(node) {
-    if (node.args.length === 0) {
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0) {
       return 'there is nothing to compare'
     }
-    if (node.args.length === 1) {
+    if (arguments.length === 1) {
       return 'pass two arguments'
     }
-    return node.args[0] === node.args[1] ? 'yes' : 'no'
+    return arguments[0] === arguments[1] ? 'yes' : 'no'
   }
 
   FunctionDictionary.prototype.isLessThan = function(node) {
-    if (node.args.length === 0) {
+    const arguments = this._convertVariablesToValues(node.args)
+    if (arguments.length === 0) {
       return 'there is nothing to compare'
     }
-    if (node.args.length === 1) {
+    if (arguments.length === 1) {
       return 'pass two arguments'
     }
-    return node.args[0] < node.args[1] ? 'yes' : 'no'
+    return arguments[0] < arguments[1] ? 'yes' : 'no'
   }
 
   FunctionDictionary.prototype.if = function(node) {
-    if ( node.args[0] === 'yes') {
-      return node.args[1]
+    const arguments = this._convertVariablesToValues(node.args)
+    if ( arguments[0] === 'yes') {
+      return arguments[1]
     } else {
-      return node.args[2]
+      return arguments[2]
     }
   }
 
