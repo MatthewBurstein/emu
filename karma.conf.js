225ff96a9dccb73b7bb58fb35e5f1bda61ef8526
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-11 11:46:12 +0100

    removed vacuous comments from karma conf file

--- karma.conf.js
+++ karma.conf.js
@@ -11,17 +11,12 @@ module.exports = function (config) {
       'src/interface.js',
     ],
 
-    // coverage reporter generates the coverage
     reporters: ['progress', 'coverage'],
 
     preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
       'src/*.js': ['coverage']
     },
 
-    // optionally, configure the reporter
     coverageReporter: {
       type: 'html',
       dir: 'coverage/'
