87a956ae26f6d9a6e6f20d0ce52f92d0e7fbbc59
Laura Chan <laurachan205@gmail.com>
2018-04-11 16:11:35 +0100

    Clean and refactor interpretSpec.js

--- spec/interpretSpec.js
+++ spec/interpretSpec.js
@@ -1,22 +1,15 @@
 describe('interpret()', function() {
-
   let intNode1, intNode2, intNode3
 
   beforeEach(() => {
     intNode1 = { }
     intNode2 = { }
     intNode3 = { }
-    intNode4 = { }
-
     spyOn(functionDictionary, 'add').and.returnValue('added')
+    spyOn(functionDictionary, 'subtract').and.returnValue('subtracted')
   })
 
-  afterEach(() => {
-    functionDictionary.add.calls.reset()
-  })
-
-  describe('when passed three-node AST', () => {
-
+  describe('when passed a node with two child nodes', () => {
     beforeEach(() => {
       let funcNode = {
         data: 'add',
@@ -26,13 +19,13 @@ describe('interpret()', function() {
       tree = [funcNode]
     })
 
-    it('calls the right function data on the dictionary', () => {
+    it('calls the named function from the dictionary', () => {
       expect(interpret(tree)).toEqual(['added'])
     })
 
   })
 
-  describe('when passed five-node nested AST', () => {
+  describe('when passed a node with a function node as a child', () => {
     beforeEach(() => {
       let innerFuncNode = {
         data: 'add',
@@ -40,68 +33,35 @@ describe('interpret()', function() {
         children: [intNode2, intNode3]
       }
       let outerFuncNode = {
-        data: 'add',
+        data: 'subtract',
         type: 'function',
         children: [intNode1, innerFuncNode]
       }
       tree = [outerFuncNode]
+      interpret(tree)
     })
 
-    it('recursively resolves functions', () => {
-      expect(interpret(tree)).toEqual(['added'])
+    it('calls the inner function', () => {
+      expect(functionDictionary.add).toHaveBeenCalled()
     })
 
+    it('calls the outer function', () => {
+      expect(functionDictionary.subtract).toHaveBeenCalled()
+    })
   })
 
-  describe('it interprets a while loop', () => {
-    xit('calls interpretLoop method', () => {
-      let funcNode1 = { data: 'say', type: 'function', children: [ 'hello world'] }
-      let funcNode2 = { data: 'say', type: 'function', children: ['bye world']}
-      let loopNode = {
+  describe('when passed a loop node', () => {
+    it('processes the second argument while the first argument evaluates to "yes"', () => {
+      let funcNode1 = { data: 'say', type: 'function', children: ['hello world'] }
+      let funcNode2 = { data: 'say', type: 'function', children: ['bye world'] }
+      const loopNode = {
         type: 'loop',
         children: [funcNode1, funcNode2]
       }
       tree = [loopNode]
-
       spyOn(functionDictionary, 'say').and.returnValues('yes', 'no')
-
-      // const spyInterpretLoop = jasmine.createSpy('interpretLoop').and.returnValue('success')
-      // let funcNode1 = {
-      //   type: 'function'
-      // }
-      // let funcNode2= {
-      //
-      // }
-      // let whileNode = {
-      //   type: 'loop',
-      //   data: 'while',
-      //   args: []
-      // }
-
 
       expect(interpret(tree)).toEqual([['no']])
     })
-
-  })
-
-})
-
-describe('interpretLoop', () => {
-  // emu('assignVariable<"x" 1>')
-  // 'while<isLessThan<x 3> returnFirst<say<x> assignVariable<x add<x 1>>>>'
-  let variableNode = VariableNode.new('x')
-  let threeNode = IntegerNode.new(3)
-  let isLessThanNode = FunctionNode.new('isLessThan', 'function', [variableNode, threeNode])
-
-  let oneNode = IntegerNode.new(1)
-  let addNode = FunctionNode.new('add', 'function', [variableNode, oneNode])
-  let sayNode = FunctionNode.new('say', 'function', [variableNode])
-
-  let assignVariableNode = FunctionNode.new('assignVariable', 'function', [variableNode, addNode])
-  let returnFirstNode = FunctionNode.new('returnFirst', 'function', [sayNode, assignVariableNode])
-  let whileNode = FunctionNode.new('while', 'loop', [isLessThanNode, returnFirstNode])
-
-  xit('evaluates a simple while node', () => {
-    expect(interpretLoop(whileNode)).toEqual(['1', '2'])
   })
 })
